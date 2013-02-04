e7f9502c3f2471c305c1b5ec1fd4a6a3fc533943
C. Scott Ananian <cscott@cscott.net>
2013-02-04 16:50:57 -0500

    Add test cases; improve node packaging; package for volo.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
 var assert = require("assert");
-var Instaview = require('../');
+var InstaView = require('../');
 var fs = require('fs');
 
 describe('basic functionality', function() {
-    it('should export an object named Instaview', function() {
-        assert.equal(!!Instaview, true);
+    it('should export an object named InstaView', function() {
+        assert.equal(!!InstaView, true);
+    });
+    ['Fingerprint', 'Earth', 'Ethiopia'].forEach(function(title) {
+        it('should properly render '+title, function() {
+            var mw = fs.readFileSync('test/'+title+'.mw', 'utf-8');
+            var ref = fs.readFileSync('test/'+title+'.html', 'utf-8');
+            var rendered = InstaView.convert(mw);
+            assert.equal(rendered, ref);
+        });
     });
 });
